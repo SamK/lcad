c69dd85af92b1b3daf04a96d6382cc29241a3bf7
Samuel Krieg <samuel.krieg+github@gmail.com>
2024-08-14 10:24:07 +0200

    feat(lines): add test for the "lines" format

---
+++ tests/parsers/test_lines.py
@@ -0,0 +1,17 @@
+import io
+import yaml
+import lcad.parsers.lines
+from . import file_io
+
+
+def test_load_list():
+    reference = yaml.load(file_io('list.yaml'), Loader=yaml.SafeLoader)
+    loaded = lcad.parsers.lines.load(file_io('list.lines'))
+    assert reference == loaded
+
+
+def test_dump_list():
+    reference = file_io('list.lines').read()
+    loaded = yaml.load(file_io('list.yaml'), Loader=yaml.SafeLoader)
+    dumped = lcad.parsers.lines.dump(loaded)
+    assert reference == dumped
--- tests/test_parserlib.py
+++ tests/test_parserlib.py
@@ -1,11 +1,18 @@
 import lcad.parserlib
 
 def test_discover_parsers():
+    """Ensure the parsers exist"""
+    # discovers the parsers
     parsers = lcad.parserlib.discover_parsers()
     assert isinstance(parsers, dict)
+
+    # make sure these parsers exist
     assert 'json' in parsers
     assert 'yaml' in parsers
     assert 'zero' in parsers
+    assert 'lines' in parsers
+
+    # make sure "load" and "dump" exist (or not) based on the documentation
     assert parsers['json']['dump'] is True
     assert parsers['json']['load'] is True
     assert parsers['zero']['dump'] is True
@@ -14,3 +21,5 @@ def test_discover_parsers():
     assert parsers['fail']['load'] is True
     assert parsers['vertical']['dump'] is True
     assert parsers['vertical']['load'] is False
+    assert parsers['lines']['dump'] is True
+    assert parsers['lines']['load'] is True
